3152c377691ba54a9ed50c8bf600da2df7f9183e
Dmitry-Lipinskiy <Dimalipa@mail.ru>
2023-07-14 23:27:09 +0300

    Updates

---
+++ static/js/594.e323ed17.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmarvel=self.webpackChunkmarvel||[]).push([[594],{9594:function(e,t,n){n.r(t);var l=n(1087),i=n(9613),r=n(184);t.default=function(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)(i.B,{}),(0,r.jsx)("p",{style:{textAlign:"center",fontWeight:"bold",fontSize:"24px"},children:"Page doesn't exist"}),(0,r.jsx)(l.rU,{style:{display:"block",textAlign:"center",fontWeight:"bold",fontSize:"24px",marginTop:"30px",color:"#9f0013"},to:"/",children:"Back to main page"})]})}}}]);
+//# sourceMappingURL=594.e323ed17.chunk.js.map
